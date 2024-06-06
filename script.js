7321d0b24b8e09cfd80b752cfc3918a970a1ef03
Yann <Yann.armstrong@stu.ocsb.ca>
2024-06-06 14:05:03 -0400

    Done 4.4

--- script.js
+++ script.js
@@ -7,10 +7,21 @@
 
 // Example
 let clicks = 0;
-document.getElementById("add_click_btn").addEventListener("click", clicks + 1)
+document.getElementById("add_click_btn").addEventListener("click", click_add)
 
-document.getElementById("min_click_btn").addEventListener("click", clicks - 1)
+document.getElementById("min_click_btn").addEventListener("click", click_min)
 
-document.getElementById("rst_click_btn").addEventListener("click", clicks = 0)
+document.getElementById("rst_click_btn").addEventListener("click", click_rst)
 
-document.getElementById("click_count").textContent = clicks
+function click_add(){
+    document.getElementById("click_count").textContent = clicks+1;
+    clicks++;
+}
+function click_min(){
+    document.getElementById("click_count").textContent = clicks-1;
+    clicks--;
+}
+function click_rst(){
+    document.getElementById("click_count").textContent = clicks=0;
+    clicks=0;
+}
